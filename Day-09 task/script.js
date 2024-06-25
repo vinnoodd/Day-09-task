4366fc9ba8cbba39d2e11ca0f873877a6327e256
vinnoodd <145673407+vinnoodd@users.noreply.github.com>
2024-06-25 20:47:58 +0530

    Add files via upload

---
+++ Day-09 task/script.js
@@ -0,0 +1,59 @@
+const hell = document.querySelector(".hell");
+
+
+setTimeout(function () {
+    hell.innerText = "10";
+    setTimeout(function () {
+        hell.innerText = "9";
+        setTimeout(function () {
+
+            hell.innerText = "8";
+            setTimeout(function () {
+
+                hell.innerText = "7";
+                setTimeout(function () {
+
+                    hell.innerText = "6";
+                    setTimeout(function () {
+
+                        hell.innerText = "5";
+                        setTimeout(function () {
+
+                            hell.innerText = "4";
+                            setTimeout(function () {
+
+                                hell.innerText = "3";
+                                setTimeout(function () {
+
+                                    hell.innerText = "2";
+                                    setTimeout(function () {
+
+                                        hell.innerText = "1";
+                                        setTimeout(function () {
+
+                                            hell.innerText = "Happy Independence Day!";
+
+                                        }, 1000);
+
+
+                                    }, 1000);
+
+
+                                }, 1000);
+
+
+                            }, 1000);
+
+                        }, 1000);
+
+                    }, 1000);
+
+                }, 1000);
+
+            }, 1000);
+
+        }, 1000);
+    }, 1000);
+}, 1000);
+
+// console.log("Welcome to loupe.");
